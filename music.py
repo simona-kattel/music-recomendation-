ddf49d63a901ef2af5c1fb23745d9e70a1c7cf91
simona-kattel <kattelsimona@gmail.com>
2025-03-10 20:09:35 +0545

    2nd commit

--- music.py
+++ music.py
@@ -5,3 +5,10 @@ genre_df = pd.read_csv("genre_music.csv" )
 
 print(df.head())
 print(genre_df.head())
+
+
+x= dataset.iloc[].values
+y= dataset.iloc[].values
+
+from sklearn.model_selection import train_test_split
+x_train , x_test, y_train, y_split = train_test_split(x,y,test_size=0.2,random_state=42)
